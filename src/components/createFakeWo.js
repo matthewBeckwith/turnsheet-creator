d4969e4fce07919f2061e15e5c9848ad9ce5d68a
Matthew Beckwith <kodahscripts@outlook.com>
2021-02-22 01:00:26 -0500

    making fake wo for testing

---
+++ src/components/createFakeWo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import firebase from "../services/firebase";
+
+const unitNumberOptions = [
+    '1234', '4561', '7894', '1597', '2356', '6542', '7865', '4578', '8985', '3565', '5412', '8451', '4124', '8524', '6532', '4573', '5288', '1124', '2411'
+];
+const unitStreetOptions = [
+    'Fake', 'Nerd', 'Western', 'Main', 'Wicked', 'Reckless', 'BoomBoom', 'Makers', 'Anus', 'Armorstand', 'Pencildick', 'Limitless', 'Whoopsie', 'Biscuit', 'Cement', 'Oak Tree', 'Fortnite', 'Gucci', 'Pepsi'
+];
+const streetTypeOptions = [
+    'St.', 'Ave.', 'Blvd.', 'Rd.', 'Ct.', 'Way'
+];
+const zipcodeOptions = [
+    '12345', '55555', '33221', '55647', '77894', '44411'
+];
+const priceOptions = [
+    125.50, 300.24, 1000.00, 850.00, 672.30, 100.00, 532.22
+]
+export default function CreateFakeWo() {
+    return (
+        <div>
+
+        </div>
+    )
+}
