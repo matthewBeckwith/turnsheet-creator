2008e8b05bcff80b771d528de51d349aa56f5f35
Matthew Beckwith <kodahscripts@outlook.com>
2021-03-03 00:08:15 -0500

    more resstructuring

--- src/pages/turnsheet2/turnsheet_pg.jsx
+++ src/pages/turnsheet2/turnsheet_pg.jsx
@@ -1,29 +1,36 @@
-// This is where I'd like to redesign the turnsheet process
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import firebase from '../../services/firebase';
+import firebase from "../../services/firebase";
 
 export default function TurnsheetPg() {
-    const { ID } = useParams();
-    const [turnsheetID, setTurnsheetID] = useState('');
-    
-    
-    useEffect(() => {
-        const db = firebase.database().ref('turnsheets');
-        if(ID){
-            setTurnsheetID(ID);
-        }else{
-            const initID = db.push().then(ref => {
-                setTurnsheetID(ref.key)
-            });
-        }
-    },[]);
+  const { ID } = useParams();
+  const [turnsheetID, setTurnsheetID] = useState("");
+  const [rooms, setRooms] = useState([]);
+  const [turnsheet, setTurnsheet] = useState({});
 
-    console.log("ID: ", turnsheetID);
-    return (
-        <div>
-            
-            New Turnsheet Page
-        </div>
-    )
+  const getRooms = (id) => {
+    return <p>Boom</p>;
+  };
+
+  useEffect(() => {
+    if (ID) {
+      setTurnsheetID(ID);
+    } else {
+      firebase
+        .database()
+        .ref("turnsheets")
+        .push()
+        .then((ref) => {
+          setTurnsheetID(ref.key);
+        });
+    }
+  }, []);
+
+  return (
+    <div>
+      {ID && <h3>{`Turnsheet ID: ${turnsheetID}`}</h3>}
+      {!ID && <h3>{`Created Turnsheet ID: ${turnsheetID}`}</h3>}
+      {getRooms(turnsheetID)}
+    </div>
+  );
 }
