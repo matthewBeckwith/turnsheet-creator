ea35b0f4c179ca8239daad6fda2fb7656668c2e3
Matthew Beckwith <kodahscripts@outlook.com>
2021-04-01 16:30:22 -0400

    still redesigning data flow

--- src/app3.jsx
+++ src/app3.jsx
@@ -1,47 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     useParams
   } from "react-router-dom";
+
+import firebase from './services/firebase';
 import useCreateKey from './hooks/useCreateKey';
 
 const Turnsheets = () => {
-    return (
-        <div>
-            <p>Turnsheets Grouped by Year!</p>
-        </div>
-    )
+    return <p>Turnsheets</p>
 }
 
 const Turnsheet = () => {
     const {ID} = useParams();
-    const uniqueID = useCreateKey();
-    const todaysDate = new Date().toTimeString();
+    const uniqueID = useCreateKey(); 
 
+    const [turnsheetID, setTurnsheetID] = useState(null);
     const [address, setAddress] = useState(null);
-    const [secDeposit, setSecDeposit] = useState(null);
-    const [ownerBalance, setOwnerBalance] = useState(null);
+    const [grandTotal, setGrandTotal] = useState(null);
+    const [requiresInvestment, setRequiresInvestment] = useState(false);
+    const [ownerSubTotal, setOwnerSubTotal] = useState(null);
+    const [tenantSubTotal, setTenantSubTotal] = useState(null);
     const [rooms, setRooms] = useState(null);
-    const [grandTotal, setGrandTotal] = useState(0);
-    const [ownerSubTotal, setOwnerSubTotal] = useState(0);
-    const [tenantSubTotal, setTenantSubTotal] = useState(0);
-    const [requiresInvestment, toggleRequiresInvestment] = useState(false);
 
-    const handleChangeAddress = (e) => {}
+    // this is throwing errors but I'm thinking something like this... parse the data into state,
+    // work with it then send back to firebase when complete as an object or couple objects if I
+    // flatten the DB
 
-    const handleChangeDeposit = (e) => {}
-
-    const handleChangeOwnerBalance = (e) => {}
-
-    const handleToggleInvestment = () => {
-        toggleRequiresInvestment(!requiresInvestment);
-    }
+    useEffect(() => {
+        if(ID){
+            firebase.database().ref('turnsheets').once('value', snapshot => {
+                snapshot.hasChild(ID)
+            }) && firebase.database().ref(`turnsheets/${ID}`).once('value', snapshot => {
+                setTurnsheetID(ID);
+                setAddress(snapshot.val().address);
+                setGrandTotal(snapshot.val().estimated_grand_total);
+                setRequiresInvestment(snapshot.val().requires_investment);
+                setOwnerSubTotal(snapshot.val().owner_subtotal);
+                setTenantSubTotal(snapshot.val().tenant_subtotal);
+                setRooms(snapshot.val().rooms);
+            })
+        }else{
+            setTurnsheetID(uniqueID);
+                setGrandTotal(0);
+                setOwnerSubTotal(0);
+                setTenantSubTotal(0);
+                setRequiresInvestment(true);
+        }
+        
+    }, [ID, uniqueID]);
 
     return (
         <div>
-            <p>{ID ? `Edit Turnsheet ${ID}` : `New Turnsheet ${uniqueID}`}</p>
+            <p>{ID ? `Edit` : `New`} Turnsheet {turnsheetID}</p>
+            {address ? <p>{address}</p> : <p>No Address</p>}
+            <p>Grand Total: {grandTotal}</p>
+            <p>Owner Total: {ownerSubTotal}</p>
+            <p>Tenant Total: {tenantSubTotal}</p>
         </div>
     )
 }
